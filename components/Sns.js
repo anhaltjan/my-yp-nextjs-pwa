1dd83a57c61428fe4da81858db410701f3402060
Jan Anhalt <77264975+anhaltjan@users.noreply.github.com>
2022-08-19 13:23:52 +0900

    updated SNS - LinkedIn

--- components/Sns.js
+++ components/Sns.js
@@ -4,6 +4,7 @@ import { BsTwitter } from 'react-icons/bs'
 import { BsInstagram } from 'react-icons/bs'
 import { BsLine } from 'react-icons/bs'
 import { BsYoutube } from 'react-icons/bs'
+import { BsLinkedin } from 'react-icons/bs'
 
 import styles from '../styles/sns.module.css'
 
@@ -12,28 +13,36 @@ const Sns = () => {
     <div>
       <ul className={styles.snsContainer}>
         <li>
-          <a href={``} target={`_blank`}>
-            <BsYoutube size='1.5rem' />
+          <a
+            href={`https://www.youtube.com/channel/UC2cN1dX5YrtWdmLbqWJ7teg`}
+            target={`_blank`}
+          >
+            <BsYoutube size='1.5em' />
           </a>
         </li>
         {/*         <li>
           <a href={``} target={`_blank`}>
-            <BsLine size='1.5rem' />
+            <BsLine size='1.5em' />
           </a>
         </li> */}
         <li>
           <a href={`https://www.facebook.com/youpoint.offical.jp`}>
-            <BsFacebook size='1.5rem' />
+            <BsFacebook size='1.5em' />
           </a>
         </li>
         <li>
           <a href={`https://www.instagram.com/youpoint_/`} target={`_blank`}>
-            <BsInstagram size='1.5rem' />
+            <BsInstagram size='1.5em' />
           </a>
         </li>
         <li>
           <a href={`https://twitter.com/youpoint_`} target={`_blank`}>
             <BsTwitter size='1.75em' />
+          </a>
+        </li>
+        <li>
+          <a href={`www.linkedin.com/in/youpoint`} target={`_blank`}>
+            <BsLinkedin size='1.75em' />
           </a>
         </li>
       </ul>
